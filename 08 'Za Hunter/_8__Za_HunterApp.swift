92f8c57d57a2eb51ab8cdad68bba887efd8cc3bf
ihateuu <replacement506@gmail.com>
2021-07-31 23:09:39 +0300

    Initial Commit

---
+++ 08 'Za Hunter/_8__Za_HunterApp.swift
@@ -0,0 +1,17 @@
+//
+//  _8__Za_HunterApp.swift
+//  08 'Za Hunter
+//
+//  Created by Kareem Almomen on 7/31/21.
+//
+
+import SwiftUI
+
+@main
+struct _8__Za_HunterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
